66b5d2714b1fa0c6f4c8709b77fa877784d730c6
thiago.procorio <thiago.procorio@TI002800>
2017-04-04 16:41:35 -0400

    .

---
+++ K19/K19/Controllers/K19Controller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace K19.Controllers
+{
+    public class K19Controller : Controller
+    {
+        // GET: K19
+        public ActionResult Index()
+        {
+            Random random = new Random();
+            ViewBag.NumeroDaSorte = random.Next();
+            return View();
+        }
+    }
+}
