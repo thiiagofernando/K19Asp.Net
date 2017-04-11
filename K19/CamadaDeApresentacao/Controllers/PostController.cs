34854b6c26cda8566429f3cc02a5003c191ae3d0
thiago.procorio <thiago.procorio@TI002800>
2017-04-11 16:55:37 -0400

    2

---
+++ K19/CamadaDeApresentacao/Controllers/PostController.cs
@@ -0,0 +1,36 @@
+﻿using CamadaDeApresentacao.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CamadaDeApresentacao.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: Post
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Lista()
+        {
+            ICollection<Post> posts = new List<Post>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                posts.Add(new Post
+                {
+                    Autor = "Autor do post " + i,
+                    Categoria = "Categoria " + i ,
+                    Data = DateTime.Now.Subtract(TimeSpan.FromDays(i)),
+                    Texto = i + " - Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                    Titulo = "Titulo do post" + i
+                });
+            }
+            return View(posts);
+        }
+    }
+}
---
+++ K19/CamadaDeApresentacao/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CamadaDeApresentacao.Models
+{
+    public class Post
+    {
+        public string Autor { get; set; }
+        public DateTime Data { get; set; }
+        public string Titulo { get; set; }
+        public string Categoria { get; set; }
+        public string Texto { get; set; }
+    }
+}
